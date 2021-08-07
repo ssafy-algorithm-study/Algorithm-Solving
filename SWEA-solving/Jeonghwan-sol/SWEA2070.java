67dea0172b53401d66a23b15e53519a2620c81c1
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-08-07 11:48:47 +0900

    08.07. study

---
+++ SWEA-solving/Jeonghwan-sol/SWEA2070.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SWEA2070 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int tc = 1; tc <= T; tc++) {
+			char res = ' ';
+			int n1 = sc.nextInt();
+			int n2 = sc.nextInt();
+			
+			if (n1 > n2) {
+				res = '>';
+			} else if (n1 < n2) {
+				res = '<';
+			} else {
+				res = '=';
+			}
+			
+			System.out.println("#" + tc + " "+ res);
+		}
+		
+		sc.close();
+	}
+}
