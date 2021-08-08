dd00580576964ed17670fd41ca42487bebf3b81a
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-08-08 11:04:33 +0900

    08.08. study

---
+++ SWEA-solving/Jeonghwan-sol/SWEA2068.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SWEA2068 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int tc = 1; tc <= T; tc++) {
+			int max = 0;
+			
+			for(int i = 0; i < 10; i++) {
+				int tmp = sc.nextInt();
+			
+				if (max < tmp) {
+					max = tmp;
+				}
+			}
+			
+			System.out.println("#" + tc + " " + max);
+		}
+		sc.close();
+	}
+}
