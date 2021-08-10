9580908de232f99229c85a6f718cf066ac201709
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-08-10 22:24:00 +0900

    08.10. study

---
+++ SWEA-solving/Jeonghwan-sol/D1/SWEA2046.java
@@ -0,0 +1,17 @@
+package D1;
+
+import java.util.Scanner;
+
+public class SWEA2046 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			System.out.print("#");
+		}
+		
+		sc.close();
+	}
+}
