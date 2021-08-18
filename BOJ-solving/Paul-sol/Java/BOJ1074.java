9d0f123a9fd306dc27141730eea318e091d0d1b7
kbu715 <kbu715@gmail.com>
2021-08-18 21:06:10 +0900

    08.18. study

--- BOJ-solving/Paul-sol/Java/BOJ1074.java
+++ BOJ-solving/Paul-sol/Java/BOJ1074.java
@@ -4,16 +4,14 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 시간초과 
-
 public class BOJ1074 {
 
 	static int N;
 	static int cnt;
 	static int r;
 	static int c;
-	static int[] dx = { 0, 0, 1, 1 }; // row 변화값 배열
-	static int[] dy = { 0, 1, 0, 1 }; // col 변화값 배열
+	static int[] dx = { 0, 0, 1, 1 }; // z 방향으로 이동하기 위한 변동값 배열
+	static int[] dy = { 0, 1, 0, 1 };
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,8 +25,8 @@ public class BOJ1074 {
 
 	}
 
-	private static void search(int N, int row, int col) {
-		if (N == 2) { // 최소 2x2 기저조건
+	private static void search(int n, int row, int col) {
+		if (n == 2) { // 최소 2x2 기저조건
 			for (int i = 0; i < 4; i++) {
 				int nr = row + dx[i];
 				int nc = col + dy[i];
@@ -40,10 +38,21 @@ public class BOJ1074 {
 			}
 			return;
 		}
-		search(N / 2, row, col); // 왼쪽 상단
-		search(N / 2, row, col + N / 2); // 오른쪽 상단
-		search(N / 2, row + N / 2, col); // 왼쪽 하단
-		search(N / 2, row + N / 2, col + N / 2); // 오른쪽 하단
+		int mid = n / 2;
+		int size = mid * mid; // 1사분면 사이즈 => 그만큼 cnt++
+
+		if (r < row + mid && c < col + mid) {
+			search(mid, row, col); // 왼쪽 상단
+		} else if (r < row + mid && c >= col + mid) {
+			cnt += size * 1;
+			search(mid, row, col + mid); // 오른쪽 상단
+		} else if (r >= row + mid && c < col + mid) {
+			cnt += size * 2;
+			search(mid, row + mid, col); // 왼쪽 하단
+		} else {
+			cnt += size * 3;
+			search(mid, row + mid, col + mid); // 오른쪽 하단
+		}
 	}
 
 }
