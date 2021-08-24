634da00f1601436872ee732dee26e9ee7fd5b4c6
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-08-24 20:02:33 +0900

    Revise : BOJ10163 코드 수정

--- BOJ-solving/Jeonghwan-sol/Java/Bronze/BOJ10163.java
+++ BOJ-solving/Jeonghwan-sol/Java/Bronze/BOJ10163.java
@@ -5,31 +5,19 @@ public class BOJ10163 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		final int M_SIZE = 1001;
-		int N = sc.nextInt();
 		int[][] map = new int[M_SIZE][M_SIZE];
-		int[][] pos = new int[N][2];
-		int[][] size = new int[N][2];
+		int N = sc.nextInt();
 		int[] res = new int[N];
 		
-		for (int nc = 0; nc < N; nc++) {
-			for (int i = 0; i < 2; i++) {
-				pos[nc][i] = sc.nextInt();
-			}
+		for (int nc = 1; nc <= N; nc++) {
+			int column = sc.nextInt();
+			int row = sc.nextInt();
+			int w = sc.nextInt();
+			int h = sc.nextInt();
 			
-			for (int j = 0; j < 2; j++) {
-				size[nc][j] = sc.nextInt();
-			}
-		}
-		
-		for (int k = 0; k < N; k++) {
-			int startR = pos[k][0];
-			int endR = pos[k][0] + size[k][0];
-			int startC = pos[k][1];
-			int endC = pos[k][1] + size[k][1];
-			
-			for (int i = startR; i < endR; i++) {
-				for (int j = startC; j < endC; j++) {
-					map[i][j] = k + 1;
+			for (int i = row; i < (row + h); i++) {
+				for (int j = column; j < (column + w); j++) {
+					map[i][j] = nc;
 				}
 			}
 		}
